3cdab39419edb6ce39ad0bcef0decf088e176fba
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-04-30 19:03:38 -0400

    lints

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::{fs::read_to_string, collections::{HashMap, VecDeque}};
+use std::{
+    collections::{HashMap, VecDeque},
+    fs::read_to_string,
+};
 
 fn main() {
     let now = std::time::Instant::now();
+
     let numbers = read_to_string("./challenge_input.txt").unwrap();
     let numbers: Vec<u128> = numbers.lines().map(|n| n.parse().unwrap()).collect();
-
     let mut map: HashMap<u128, VecDeque<usize>> = HashMap::new();
 
-    for n in 0..100 {
-        match map.get_mut(&numbers[n]) {
-            Some(v) => v.push_back(n),
-            None => { map.insert(numbers[n], VecDeque::from([n])); },
+    for (i, n) in numbers.iter().enumerate().take(100) {
+        match map.get_mut(n) {
+            Some(v) => v.push_back(i),
+            None => {
+                map.insert(*n, VecDeque::from([i]));
+            }
         }
     }
 
@@ -19,24 +24,27 @@ fn main() {
     for n in 100..numbers.len() {
         let mut safe = false;
 
-        for p in &numbers[n-100..n] {
+        for p in &numbers[n - 100..n] {
             let difference = numbers[n].saturating_sub(*p);
 
             if difference == 0 {
                 continue;
             }
 
-            match map.get(&difference) {
-                Some(_) => { safe = true; break; },
-                None => (),
+            if let Some(v) = map.get(&difference) {
+                if !v.is_empty() {
+                    safe = true;
+                    break;
+                }
             }
         }
 
-        map.get_mut(&numbers[n-100]).unwrap().pop_front();
-
+        map.get_mut(&numbers[n - 100]).unwrap().pop_front();
         match map.get_mut(&numbers[n]) {
             Some(v) => v.push_back(n),
-            None => { map.insert(numbers[n], VecDeque::from([n])); },
+            None => {
+                map.insert(numbers[n], VecDeque::from([n]));
+            }
         }
 
         if !safe {
@@ -44,6 +52,6 @@ fn main() {
         }
     }
 
-    println!("{:?}", mines);
+    println!("Answer: {mines:?}");
     println!("Done in {} Nanos", now.elapsed().as_nanos());
 }
